7e92057ac12479ccc823a674a50cd9a80eea7b6d
Khooshal Seechurn <khooshalseechurn@gmail.com>
2022-11-12 16:59:26 -0500

    created index, show and new routes controller and views

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,26 @@
+class TopicsController < ApplicationController
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(params(topic_params))
+    @topic.save
+    redirect_to topics_path
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "topics", to: "topics#index"
+  get "topics/new", to: "topics#new"
+  post "topics", to: "topics#create"
+  get "topics/:id", to: "topics#show"
 end
